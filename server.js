8786bf9520214656fb8d6fbaf211f01dc75e61ba
Darragh Ferry <darraghferry@hotmail.com>
2020-01-23 20:17:32 +0000

    created endpoint for get recipes

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require("express");
 
-// const RecipeRouter
+const RecipeRouter = require("./food/recipes-router");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/recipes", RecipeRouter);
+
+module.exports = server;
